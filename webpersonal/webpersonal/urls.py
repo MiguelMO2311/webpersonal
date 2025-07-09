2fee7b17cc11b866ec3c1c9407ca35026d09ea17
MiguelMO2311 <mmeneses73@gmail.com>
2025-07-10 00:50:13 +0200

    wsgi

--- webpersonal/webpersonal/urls.py
+++ webpersonal/webpersonal/urls.py
@@ -4,9 +4,9 @@ from django.conf import settings
 
 from core import views as core_views
 from portfolio import views as portfolio_views
-from core.views import home, send_email
 
-
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('', core_views.home, name="home"),
@@ -17,6 +17,7 @@ urlpatterns = [
     path("contact/send_email/", core_views.send_email, name="send_email"),
     path('admin-cv/', core_views.mi_vista_admin_cv, name='cv_admin'),
     path('fsd-cv/', core_views.mi_vista_fsd_cv, name='cv_fsd'),
+    url(r'^favicon\.ico$', serve, {'path': 'static/core/favicon.ico'}),
 ]
 
 
